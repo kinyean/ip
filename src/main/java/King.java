e660062b72a4dcb272076b8e0b44380a61d70f88
King <kinyean2@gmail.com>
2026-01-21 16:07:27 +0800

    Level-1 Echo

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -1,11 +1,21 @@
-public class Duke {
+import java.util.Scanner;
+
+public class King {
     public static void main(String[] args) {
-        String l = "____________________________________________________________\n" +
+        Scanner scan = new Scanner(System.in);
+        String greet = "____________________________________________________________\n" +
                 " Hello! I'm King\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
-        System.out.println(l);
+        String exit = " Bye. Hope to see you again soon!\n";
+        System.out.println(greet);
+        while (true) {
+            String s = scan.nextLine();
+            if (s.equals("bye")) {
+                System.out.println(exit);
+                break;
+            }
+            System.out.println(s);
+        }
     }
 }
