e254fa2a390acc617dda341248bcd4fe132b6f4e
King <kinyean2@gmail.com>
2026-01-21 14:48:57 +0800

    Level-0: rename Duke to King

---
+++ src/main/java/King.java
@@ -0,0 +1,11 @@
+public class Duke {
+    public static void main(String[] args) {
+        String l = "____________________________________________________________\n" +
+                " Hello! I'm King\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(l);
+    }
+}
