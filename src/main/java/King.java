930804cac3201a5fd77971386cb97955f7bb1efb
King <kinyean2@gmail.com>
2026-01-21 17:03:38 +0800

    Level-2 Add List

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class King {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String[] sarr = new String[100];
+        int counter = 0;
         String greet = "____________________________________________________________\n" +
                 " Hello! I'm King\n" +
                 " What can I do for you?\n" +
@@ -14,8 +16,15 @@ public class King {
             if (s.equals("bye")) {
                 System.out.println(exit);
                 break;
+            } else if (s.equals("list")) {
+                for (int i = 0; i < counter; i++) {
+                    System.out.println(i + 1 + "." + sarr[i]);
+                }
+            } else {
+                sarr[counter] = s;
+                counter++;
+                System.out.println("added:" + s);
             }
-            System.out.println(s);
         }
     }
 }
